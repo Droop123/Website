ba5783f7a59d852bf052d93544d13eff54017962
Droop123 <droopk611@gmail.com>
2025-01-08 22:40:39 +0500

    Add files via upload

---
+++ Hackathon/index.js
@@ -0,0 +1,15 @@
+// // Available items in the bakery
+// const bakeryItems = ["Chips", "Snacks", "Chocolate", "Pepsi", "Nimco"];
+// function checkAvailability(userInput) {
+//     const index = bakeryItems.indexOf(userInput);
+//     if (index !== -1) {
+//         console.log(`${userInput} is available in our bakery at index ${index}`);
+//     } else {
+//         console.log(`Sorry, ${userInput} is not available in our bakery`);
+//     }
+// }
+// const userInput = prompt("What do you want? Chips, Snacks, Chocolate, Pepsi, Nimco").trim();
+// checkAvailability(userInput);
+
+
+// var items=
